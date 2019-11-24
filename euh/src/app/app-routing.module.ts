8eb81974bea54f093080112ed113c1a84052755c
Gabriel A. López López <glpz@daxslab.com>
2019-11-23 19:31:38 -0500

    ENH: cleaning

--- euh/src/app/app-routing.module.ts
+++ euh/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'list',
     pathMatch: 'full'
   },
   {
