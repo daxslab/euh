85d3c9e12ccdfd93743b5e668fdf546c37d0aad5
Gabriel A. López López <glpz@daxslab.com>
2019-11-25 13:18:05 -0500

    Revert "more ussd"
    This reverts commit cfe32d8aea6f38d60c23c435cc5f801a0a7f91ed.

--- euh/src/app/app-routing.module.ts
+++ euh/src/app/app-routing.module.ts
@@ -1,31 +1,26 @@
-import {NgModule} from '@angular/core';
-import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'list',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-    },
-    {
-        path: 'list',
-        loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-    },
-    {
-        path: 'list/:id',
-        loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-    }
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  }
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
-    ],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
